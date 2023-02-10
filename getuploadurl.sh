1bb8b20e919db27537949edd6a1db659fb7c884c
Wang Wei <wangwei1237@gmail.com>
2023-02-10 18:04:34 +0800

    update set-output

--- getuploadurl.sh
+++ getuploadurl.sh
@@ -2,7 +2,7 @@
 
 if [ $# -ne 2 ]
 then
-    echo "::error the parameters error, please check!!!"
+    #echo "::error the parameters error, please check!!!"
     exit 1
 fi
 
@@ -20,5 +20,7 @@ then
     upload_url=$(curl -X POST -H "Accept: application/vnd.github.v3+json" -H "Authorization: token ${token}" -d "{\"tag_name\":\"${version}\", \"name\":\"Build for ${version}\"}" "${create_release_url}" | grep 'upload_url' | cut -d'"' -f4)
 fi
 
+echo $upload_url
+
 #echo "::set-output name=upload-url::$upload_url"
-echo "upload-url=$upload_url" >> $GITHUB_ENV
+#echo "upload-url=$upload_url" >> $GITHUB_ENV
