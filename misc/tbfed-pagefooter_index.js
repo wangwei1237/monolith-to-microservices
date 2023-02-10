ec7f4d78eec64484e5fe8a95e7f197e20c3362de
Wang Wei <wangwei1237@gmail.com>
2023-02-10 11:25:40 +0800

    update npm version

--- misc/tbfed-pagefooter_index.js
+++ misc/tbfed-pagefooter_index.js
@@ -29,9 +29,12 @@ module.exports = {
 	  '\n\n<div id="gitalk-container" class="page-footer"></div>' +
       '\n\n<script src="https://wangwei1237.gitee.io/gittalk-config.js"></script>';
       */
+     /*
       var strComment = '\n\n<script src="//unpkg.com/valine/dist/Valine.min.js"></script>' + 
       '\n\n<div id="vcomments"></div>' + 
       '\n\n<script> new Valine({el: "#vcomments",appId: "ppRS6IT7xMHmCl54L7ynIC2Z-gzGzoHsz",appKey: "qEmM49ZlU6LOwXCHjzMUECKu"})</script>';
+      */
+     var strComment = '';
       page.content = page.content + strComment + str;
       return page;
     }
