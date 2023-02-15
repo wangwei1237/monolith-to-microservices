ac6df22e00bf60021e55eb6e9843239cccf08a31
Wang Wei <wangwei1237@gmail.com>
2023-02-15 17:43:49 +0800

    update valine

--- misc/tbfed-pagefooter_index.js
+++ misc/tbfed-pagefooter_index.js
@@ -29,11 +29,11 @@ module.exports = {
 	  '\n\n<div id="gitalk-container" class="page-footer"></div>' +
       '\n\n<script src="https://wangwei1237.gitee.io/gittalk-config.js"></script>';
       */
-     /*
+     
       var strComment = '\n\n<script src="//unpkg.com/valine/dist/Valine.min.js"></script>' + 
       '\n\n<div id="vcomments"></div>' + 
-      '\n\n<script> new Valine({el: "#vcomments",appId: "ppRS6IT7xMHmCl54L7ynIC2Z-gzGzoHsz",appKey: "qEmM49ZlU6LOwXCHjzMUECKu"})</script>';
-      */
+      '\n\n<script> new Valine({el: "#vcomments",appId: "ppRS6IT7xMHmCl54L7ynIC2Z-gzGzoHsz",appKey: "qEmM49ZlU6LOwXCHjzMUECKu", path: window.location.pathname, avatar: "mp", placeholder: "快来评论吧~", recordIP: true,visitor: true,})</script>';
+      
      var strComment = '';
       page.content = page.content + strComment + str;
       return page;
